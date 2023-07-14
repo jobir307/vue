0463d4e63752892546af8c7c2fe6be8ea7df8185
navruz22 <navruz.ochilov98@mail.ru>
2023-07-14 07:46:58 +0300

    done

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,20 @@
             v-if="!isPostsLoading"
         />
         <div v-else>Идёт загрузка</div>
+        <div ref="observer" class="observer"></div>
+        <!-- <div class="page_wrapper">
+            <div 
+                v-for="pageNumber in totalPages" 
+                :key="pageNumber"
+                class="page"
+                :class="{
+                    'current_page': pageNumber === page
+                }"
+                @click="changePage(pageNumber)"
+            >
+                {{ pageNumber }}
+            </div>
+        </div> -->
     </div>    
 </template>
 
@@ -46,6 +60,9 @@ export default {
             isPostsLoading: false,
             searchQuery: '',
             selectedSort: '',
+            page: 1,
+            limit: 10,
+            totalPages: 0,
             sortOptions: [
                 {value: 'title', name: 'По названию'},
                 {value: 'body', name: 'По описанию'},
@@ -66,14 +83,42 @@ export default {
         showDialog() {
             this.dialogVisible = true
         },
+        /* 
+        changePage(pageNumber) {
+            this.page = pageNumber
+        },*/
         async fetchPosts() {
             try {
                 this.isPostsLoading = true
                 setTimeout(async () => {
-                    const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+                    const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+                        params: {
+                            _page: this.page,
+                            _limit: this.limit
+                        }
+                    })
+                    this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
                     this.posts = response.data
-                    this.isPostsLoading = false
-                }, 1200);
+                }, 500);
+            } catch (error) {
+                console.log(error)
+            } finally {
+                this.isPostsLoading = false
+            }
+        },
+        async loadMorePosts() {
+            try {
+                this.page += 1
+                setTimeout(async () => {
+                    const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+                        params: {
+                            _page: this.page,
+                            _limit: this.limit
+                        }
+                    })
+                    this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
+                    this.posts = [...this.posts, ...response.data]
+                }, 500);
             } catch (error) {
                 console.log(error)
             }
@@ -81,14 +126,33 @@ export default {
     },
     mounted() {
         this.fetchPosts()
+        console.log(this.$refs.observer)
+        let options = {
+            root: document.querySelector("#scrollArea"),
+            rootMargin: "0px",
+            threshold: 1.0,
+        };
+        let callback = (entries, observer) => {
+            if (entries[0].isIntersecting && this.page < this.totalPages){
+                this.loadMorePosts()
+            }
+        }
+        let observer = new IntersectionObserver(callback, options);
+        observer.observe(this.$refs.observer)
+
     },
     computed: {
         sortedPosts() {
             return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
         },
         sortedAndSearchedPosts() {
-            return this.sortedPosts.filter(post => post.title.includes(this.searchQuery))
+            return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
         }
+    },
+    watch: {
+        /* page() {
+            this.fetchPosts()
+        } */
     }
 }
 </script>
@@ -99,6 +163,7 @@ export default {
     margin: 0;
     box-sizing: border-box;
 }
+
 .app {
     padding: 20px;
 }
@@ -107,5 +172,24 @@ export default {
     display: flex;
     justify-content: space-between;
     margin: 15px 0;
+}
+
+.page_wrapper {
+    display: flex;
+    margin-top: 15px;
+}
+
+.page {
+    border: 1px solid black;
+    padding: 10px;
+}
+.current_page {
+    border: 2px solid teal;
+
+}
+
+.observer {
+    height: 30px;
+    background: green;
 }
 </style>
---
+++ src/router/router.js
@@ -0,0 +1,11 @@
+
+
+const routes = [
+    {
+        path: '/',
+        component: 
+    },
+    {
+
+    }
+]
